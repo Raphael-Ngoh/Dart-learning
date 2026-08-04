2d2b1ddd44ad31daf923908371db0b711859f97e
Raphael-Ngoh <raphyngoh@gmail.com>
2026-08-04 20:15:08 +0100

    Exercise 1 about a calculator finished.

--- Exercises/01_Variables_conditionnal/Exercise1.dart
+++ Exercises/01_Variables_conditionnal/Exercise1.dart
@@ -1,6 +1,32 @@
-/*Intelligent Calculator
-*/
+import 'Dart:io';
+/*Simple Calculator - Working on variables, operators and conditions*/
 
-int main(){
-  
+
+void main(){
+  print("Give number 1");
+  var number_1 = int.parse(stdin.readLineSync()?? "0");
+  print("Give number 2");
+  var number_2 = int.parse(stdin.readLineSync()?? "0");
+  print("Choose one of the following operators : \n+ :(plus) \n- :(minus)  \n* :(multiply)\n/ :(divide)\n% :(modulo)", );
+  var opt = stdin.readLineSync();
+
+  if (opt == "+" || opt == "-" || opt == "*" || opt == "/" || opt == "%") {
+    if (opt == "+") {
+      print(number_1 + number_2);
+    } else if (opt == "-") {
+      print(number_1 - number_2);
+    } else if (opt == "*") {
+      print(number_1 * number_2);
+    } else if (opt == "/") {
+      if (number_2 == 0) {
+        print("Error ! Can't calculate $number_1 divided by 0");
+      } else {
+        print(number_1 / number_2);
+      }
+    } else if (opt == "%") {
+      print(number_1 % number_2);
+    }
+  } else {
+    print("The choosen operator is unknow. Choose +, -, *, /,or  %");
+  }
 }
