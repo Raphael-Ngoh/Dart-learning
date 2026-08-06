f144d1dd1f8b2a08ad2a6458431068e6bee8b381
Raphael-Ngoh <raphyngoh@gmail.com>
2026-08-06 10:46:13 +0100

    The same file

--- Exercises/01_Variables_conditionnal/Exercise2.dart
+++ Exercises/01_Variables_conditionnal/Exercise2.dart
@@ -8,14 +8,14 @@ The program must determine the distinction obtained.
 void main(){
 
 print("Give the student's name");
-var nameStudent = stdin.readLineSync()??"Bob";
+var nameStudent = stdin.readLineSync()??"Bob john" ;
 print("Give the student's note");
-var note = double.parse(stdin.readLineSync()??"10.0");
+var note = double.tryParse(stdin.readLineSync()??"10.0")??10.0;
 
 if(note < 10){
   print("$nameStudent Failed !");
 }else if (note >=10 && note <=11.99 ){
-  print(" $nameStudent is Average");
+  print("$nameStudent is Average");
 }else if (note >=12 && note <=13.99 ){
   print("$nameStudent pretty good");
 }else if (note >=14 && note <=15.99 ){
