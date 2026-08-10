e2f2bca9d45c751a4af7597a44a1f0d39589f4b8
Raphael-Ngoh <raphyngoh@gmail.com>
2026-08-10 12:10:41 +0100

    I have reformat the code.

--- Exercises/02_String/Exercise5.dart
+++ Exercises/02_String/Exercise5.dart
@@ -19,64 +19,64 @@ void main(List<String> args) {
   RegExp oneupper = RegExp(r'[A-Z]{1,}');
   RegExp onelower = RegExp(r'[a-z]{1,}');
   RegExp onedigit = RegExp(r'[0-9]{1,}');
-  RegExp specialchar = RegExp(r'\W+'); 
+  RegExp specialchar = RegExp(r'\W+');
 
   print("============== PASSWORD ANALYSER ==============");
-  if( numchars.hasMatch(pass)  && 
-      oneupper.hasMatch(pass) && 
-      specialchar.hasMatch(pass) && 
-      onelower.hasMatch(pass) && 
-      onedigit.hasMatch(pass)){
-        print("""_______________________________________________
+  if (numchars.hasMatch(pass) &&
+      oneupper.hasMatch(pass) &&
+      specialchar.hasMatch(pass) &&
+      onelower.hasMatch(pass) &&
+      onedigit.hasMatch(pass)) {
+    print("""_______________________________________________
 - contains at least 8 characters : YES
 - includes at least one uppercase letter : YES
 - includes at least one lowercase letter : YES
 - includes at least one digit : YES
 - includes at least one special character : YES
 _______________________________________________
-- The password '$pass' is VALID
-              """);
+- The password '$pass' is VALID """);
   } else {
-
     // Checking the length of the password
-    if (numchars.hasMatch(pass)== false){
-     print("Contains at least 8 characters : NO");
-    }else{
-     print("Contains at least 8 characters : YES");
+    if (numchars.hasMatch(pass) == false) {
+      print("Contains at least 8 characters : NO");
+    } else {
+      print("Contains at least 8 characters : YES");
     }
 
     // Checking if te password contains an Uppercase letter
-    if (oneupper.hasMatch(pass) == false){
+    if (oneupper.hasMatch(pass) == false) {
       print("Includes at least one uppercase letter  : NO");
-    }else{
-     print("Includes at least one uppercase letter : YES");
+    } else {
+      print("Includes at least one uppercase letter : YES");
     }
 
     // Checking if the password contains a lowercase letter
-    if (onelower.hasMatch(pass) == false){
+    if (onelower.hasMatch(pass) == false) {
       print("Includes at least one lowercase letter  : NO");
-    }else{
-     print("Includes at least one lowercase letter : YES");
+    } else {
+      print("Includes at least one lowercase letter : YES");
     }
 
     // Checking if password contains a digit
-    if (onedigit.hasMatch(pass) == false){
+    if (onedigit.hasMatch(pass) == false) {
       print("Includes at least one digit  : NO");
-    }else{
-     print("Includes at least one digit : YES");
+    } else {
+      print("Includes at least one digit : YES");
     }
 
     // Checking if password contains a special char
-    if (specialchar.hasMatch(pass) == false){
+    if (specialchar.hasMatch(pass) == false) {
       print("Includes at least one special character  : NO");
-    } else{
-     print("Includes at least one special character : YES");
+    } else {
+      print("Includes at least one special character : YES");
     }
 
-    print("_______________________________________________ \n- The password '$pass' is INVALID");
+    print(
+      "_______________________________________________ \n- The password '$pass' is INVALID",
+    );
   }
 
- /* 
+  /* 
 var isUpper = false;
 var isLower = false;
 var digit = false;
@@ -106,7 +106,4 @@ var digit = false;
     if(isUpper == true && isLower == true && digit == true) break;
   }
 */
-
-
-  
 }
