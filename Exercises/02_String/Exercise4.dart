450173dcb538254f43e1bf4fac7de8ab2f5558db
Raphael-Ngoh <raphyngoh@gmail.com>
2026-08-13 22:00:02 +0100

    I have just created a mini test corrector using some string methods, concatenation and loop for

--- Exercises/02_String/Exercise4.dart
+++ Exercises/02_String/Exercise4.dart
@@ -1,4 +1,14 @@
-/* Mini Test corrector */
+/* Mini Test corrector 
+You are given a poorly written sentence:
+"   bonjour   tout LE monde bienvenue CHEZ dart   "
+Your program must:
+1. remove unnecessary spaces;
+2. capitalize only the first letter of the sentence;
+3. convert all other letters to lowercase (unless you implement
+word-by-word capitalization);
+4. replace the word "dart" with "Flutter";
+5. display the final sentence.
+*/
 void main() {
   var ph = "   bonjour   tout LE monde bienvenue CHEZ dart   ";
   var phconcat = "";
@@ -14,4 +24,5 @@ void main() {
   phtrimed = phconcat.trim();
   phfinal = phtrimed[0].toUpperCase() + phtrimed.substring(1).toLowerCase();
   print(phfinal.replaceAll("dart", "Flutter"));
+  
 }
