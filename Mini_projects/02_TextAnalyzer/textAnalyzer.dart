560c28364f971c2c44e9aa33539cb61d90c9dbf3
Raphael-Ngoh <raphyngoh@gmail.com>
2026-08-16 04:35:51 +0100

    I have built 1 feature today : The transformation feature - 4/5 instructions

--- Mini_projects/02_TextAnalyzer/textAnalyzer.dart
+++ Mini_projects/02_TextAnalyzer/textAnalyzer.dart
@@ -1,5 +1,5 @@
 void main(List<String> args) {
-  var text = "Writing a text. My own text. 2025";
+  var text = "Writing a text. My own text 2026.";
   var listOfchars = text.split("");
   var listOfwords = text.split(" ");
 
@@ -8,7 +8,7 @@ void main(List<String> args) {
   var numberOfwords = 0;
   var numberOfspaces = 0;
 
-  // Checking the real words of the list without including numbers, punctuations or special caracters
+  // Checking the real word of the list without including numbers, punctuations or special caracters
   for (var word in listOfwords) {
     if (RegExp(r'[A-Za-z]{1,}').hasMatch(word)) {
       numberOfwords += 1;
@@ -22,13 +22,6 @@ void main(List<String> args) {
     }
   }
 
-  print("""
-  Original text : $text
-  Number of characters : $numberOfchars
-  Number of words : $numberOfwords
-  Number of spaces : $numberOfspaces
-  """);
-
   //2. Analyzing chars
   var numberOfupper = 0;
   var numberOflower = 0;
@@ -37,6 +30,9 @@ void main(List<String> args) {
   var numberOfvowels = 0;
   var numberOfconsons = 0;
 
+  // This loop check each chars of the text and add +1 to corresponding varaibles
+  //if the conditions are respected :
+  // Character is upper case, Lower case, a digit, a special character, a vowel or a consonnant
   for (var item in listOfchars) {
     if (RegExp(r'[A-Z]').hasMatch(item)) {
       numberOfupper += 1;
@@ -60,11 +56,73 @@ void main(List<String> args) {
   }
 
   print("""
+  
+""");
+
+  //3. Searching a word and displaying it
+  /* var wordtosearch = "Writing";
+  var wordAppearence = 0;
+  var listofword_modified = listOfwords;
+  for (var i = 0; i < listofword_modified.length; i++) {
+    if (listofword_modified[i].contains(".")) {
+      listofword_modified[i] = listofword_modified[i].substring(
+        0,
+        listofword_modified[i].indexOf("."),
+      );
+    }
+  }
+  // Checking if the word exists
+  bool wordExist = text.contains(wordtosearch) ? true : false;
+
+  if (wordExist) {
+    // Number of appearences
+    for (var word in listofword_modified) {
+      if (word.toLowerCase() == wordtosearch.toLowerCase()) {
+        wordAppearence += 1;
+      }
+    }
+  }
+  print("Number of appearance of $wordtosearch : $wordAppearence");
+
+  //Position of his first occurence
+  print(
+    "First occurence position : $wordExist ${listOfwords.indexOf(wordtosearch.toLowerCase())}",
+  );*/
+
+  //4. Transformations :
+  var textReversed = listOfchars.reversed.join("");
+  var listofcapital = listOfwords;
+  for (var i = 0; i < listofcapital.length; i++) {
+    listofcapital[i] =
+        listofcapital[i][0].toUpperCase() + listofcapital[i].substring(1);
+  }
+
+  print("""
+*****************************************************************************
+*                                                                           *
+*                           FINAL REPORT ABOUT TEXT ANALYZER                *
+*                                                                           *
+*****************************************************************************
+""");
+  print("""
+----------------------------- About Text -----------------------------------
+  Original text : $text
+  Number of characters : $numberOfchars
+  Number of words : $numberOfwords
+  Number of spaces : $numberOfspaces
+
+---------------------------- Characters Analysis ---------------------------
   Number of uppercase letter : $numberOfupper
   Number of lowercase letter : $numberOflower
   Number of digit : $numberOfdigit
   Number of special char : $numberOfspecial
   Number of vowela : $numberOfvowels
-  Number of consons : $numberOfconsons
+  Number of consonants : $numberOfconsons
+
+----------------------------- Transformations ------------------------------
+  Text in uppercase : ${text.toUpperCase()}
+  Text in lowercase : ${text.toLowerCase()}
+  Text Reversed : $textReversed
+  Text with words with uppercase letters : ${listofcapital.join(" ")}
 """);
 }
