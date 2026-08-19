a36a45f928a50a209f8a35c335d4d5950c547699
Raphael-Ngoh <raphyngoh@gmail.com>
2026-08-19 13:22:56 +0100

    Just optimized the code : Adding two variables, using if only, and using a single print function for displaying results on multi lines

--- Exercises/01_Variables_conditionnal/Exercise4.dart
+++ Exercises/01_Variables_conditionnal/Exercise4.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+
 /* CALCULATOR OF COMMERCIAL BILL
 * PURPOSE :
 A shop wants to automate the calculation of an invoice.
@@ -20,24 +21,26 @@ The program must then calculate:
 You may choose the discount rate yourself.
  */
 void main() {
-
   print("Client name :");
-  var client_name = stdin.readLineSync()??"Bob";
+  var client_name = stdin.readLineSync() ?? "Bob";
 
   print("Purchase amount :");
-  var amount = double.tryParse(stdin.readLineSync()??"0")??0;
-
-  print("Fidelity card of client :");
-  var fidelity_card = bool.tryParse(stdin.readLineSync()??"true")??true;
+  var amount = double.tryParse(stdin.readLineSync() ?? "0") ?? 0;
   
-  print("------------------- Bill of $client_name -------------------");
-  print("Initial payement : $amount FCFA");
-  if(amount > 100000 || fidelity_card == true){
-    print("Discount on items : ${amount * 20/100} FCFA");
-    print("Total to pay : ${amount - amount * 20/100} FCFA");
-  } else {
-    print("Discount on items : 0 FCFA");
-    print("Total to pay : $amount FCFA");
+  print("Fidelity card of client :");
+  final fidelity_card = bool.tryParse(stdin.readLineSync() ?? "") ?? false;
+
+  var discountAmount = 0.0; 
+  var toPay = amount;
+
+  if (amount > 100000 || fidelity_card == true) {
+    discountAmount = amount * 0.2;
+    toPay = amount - discountAmount;
   }
 
+  print("""-------------- Bill of $client_name ---------------
+Initial amount : $amount FCFA
+Discount on items : - $discountAmount FCFA
+Total to pay : $toPay FCFA
+  """);
 }
