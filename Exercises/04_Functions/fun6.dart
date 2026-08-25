830c8d799630dc919b2e99c25eab2dbede627832
Raphael-Ngoh <raphyngoh@gmail.com>
2026-08-25 19:03:04 +0100

    Fun6.dart : Part 2 - Adding new functions [ findWorstOf, findBestOf }

--- Exercises/04_Functions/fun6.dart
+++ Exercises/04_Functions/fun6.dart
@@ -1,3 +1,4 @@
+// Calculate the average of class notes
 double calculateAverage(
   Map<String, List<int>> students, {
   required String studentName,
@@ -17,6 +18,7 @@ double calculateAverage(
   return average;
 }
 
+// Determine if a student has passed or failed
 String determineStatut(
   Map<String, List<int>> students, {
   required String studentName,
@@ -25,13 +27,45 @@ String determineStatut(
     ? "Passed"
     : "Failed";
 
+// Find the best student of the class
+String findBestOf(
+  Map<String, List<int>> students, {
+  required String studentName,
+}) {
+  var bestStudent = studentName;
+  for (var student in students.keys) {
+    if (calculateAverage(students, studentName: bestStudent) <
+        calculateAverage(students, studentName: student)) {
+      bestStudent = student;
+    }
+  }
+
+  return bestStudent;
+}
+
+//Find the worst student
+String findWorstOf(
+  Map<String, List<int>> students, {
+  required String studentName,
+}) {
+  var worstStudent = studentName;
+  for (var student in students.keys) {
+    if (calculateAverage(students, studentName: worstStudent) >
+        calculateAverage(students, studentName: student)) {
+      worstStudent = student;
+    }
+  }
+
+  return worstStudent;
+}
+
 void main(List<String> args) {
   Map<String, List<int>> students = {
     "Alice": [14, 16, 12, 15],
     "Marc": [9, 11, 8, 10],
     "Paul": [17, 18, 16, 19],
   };
-  //print(calculateAverage(students, studentName: "Alice"));
-  //print(calculateAverage(students, studentName: "marc"));
+  print(findBestOf(students, studentName: "Alice"));
+  print(findWorstOf(students, studentName: "Alice"));
   print(determineStatut(students, studentName: "Marc"));
 }
