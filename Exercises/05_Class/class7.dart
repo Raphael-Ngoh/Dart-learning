4a6703d7f342876eacc38fe88e8e512462c88ec7
Raphael-Ngoh <raphyngoh@gmail.com>
2026-09-05 22:43:57 +0100

    Just starting

---
+++ Exercises/05_Class/class7.dart
@@ -0,0 +1,24 @@
+class Student {
+  
+
+}
+
+class Teacher {
+
+}
+
+class Course {
+
+}
+
+class School {
+  List<Student> listOfStudents;
+  List<Teacher> listOfTeacher;
+  List<Course> listOfCourses;
+
+
+
+}
+void main(List<String> args) {
+  
+}
